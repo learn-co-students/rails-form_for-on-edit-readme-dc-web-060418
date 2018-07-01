8a4cbf06af33b1711b7d78ae9b9211c4ad5b6cca
wley3337 <wley3337@gmail.com>
2018-07-01 16:58:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  # by adding :update in the resources array, the following line is supperfoulous 
+  # patch 'posts/:id', to: 'posts#update'
+
 end
